686e2b968de233dcea4891c21a72fffb4ecdf45b
Sandeep4856 <sandeepkumardarla001@gmail.com>
2025-07-24 18:14:30 +0530

    practice script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -5,4 +5,4 @@ NAMES=( "sandeep"  "samhith"  "sohith" )
 echo "person1 is: ${NAMES[0]}"
 echo "person2 is: ${NAMES[1]}"
 echo "person3 is: ${NAMES[2]}"
-echo "total persons is: ${NAMES[$@]}"
+echo "total persons are : ${NAMES[@]}"
