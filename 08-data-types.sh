0f8b6664b11f0bd013d4d090013bceda5fabfc20
Sandeep4856 <sandeepkumardarla001@gmail.com>
2025-07-24 18:23:14 +0530

    practice script

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,21 +1,21 @@
-
-
-# NO1=$1
-# NO2=$2
-
-# SUM=$(($NO1+$NO2))
-
-# echo " sum of $NO1 and $NO2 digits is:  $SUM "
-
-
-
-
-
 #!/bin/bash
 
 NO1=$1
 NO2=$2
 
-SUM=$(($NO1+NO2))
+SUM=$(($NO1+$NO2))
 
-echo "Total of $NO1 and $NO2 is: $SUM"
+echo " sum of $NO1 and $NO2 digits is:  $SUM "
+
+
+
+
+
+
+
+# NO1=$1
+# NO2=$2
+
+# SUM=$(($NO1+NO2))
+
+# echo "Total of $NO1 and $NO2 is: $SUM"
