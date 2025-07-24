28ff1a38c2f51bec25f46806696606ea59db5204
Sandeep4856 <sandeepkumardarla001@gmail.com>
2025-07-24 20:07:38 +0530

    shell script

---
+++ 11-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you are not a super user"
+    exit 1
+else
+    echo " you are a super user"
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.....FAILURE"
+        exit 1
+    else
+        echo "$2.....SUCCESS"
+}
+
+dnf install mysql -y
+VALIDATE $? "installing MYSQL" 
+# $? is considered as $1 -->  1st variable  || "installing MYSQL" is considered as $2 -->  2nd  variable
+
+
+dnf install git -y
+VALIDATE $? "installing GITL" 
+# $? is considered as $1 -->  1st variable  || "installing git" is considered as $2 -->  2nd  variable
