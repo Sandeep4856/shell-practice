aa31fd70fb56ce7607d2ba9c0f1c856d5ea06880
Sandeep4856 <sandeepkumardarla001@gmail.com>
2025-07-24 20:37:05 +0530

    shell script

--- 12-colors.sh
+++ 12-colors.sh
@@ -4,7 +4,9 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then 
@@ -18,10 +20,10 @@ fi
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo  "$2.....  FAILURE"
+        echo  -e "$2..... $R FAILURE $N "
         exit 1
     else
-        echo  "$2.....SUCCESS"
+        echo  -e "$2.....$G SUCCESS $N "
     fi
 }
 
@@ -35,5 +37,5 @@ VALIDATE $? "installing GIT"
 # $? is considered as $1 -->  1st variable  || "installing git" is considered as $2 -->  2nd  variable
 
 
-dnf install git -y &>>$LOGFILE
+dnf install dockerr -y &>>$LOGFILE
 VALIDATE $? "installing GIT" 
